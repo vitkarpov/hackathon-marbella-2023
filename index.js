938a0a2c32745754e102b82ed6a6ffbdfd034c5f
Viktor Karpov <viktor.karpov@messagebird.com>
2023-11-08 11:44:41 +0100

    API

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 const express = require('express')
 const app = express()
 const port = 3000
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  PutCommand,
+  GetCommand,
+  UpdateCommand,
+} from "@aws-sdk/lib-dynamodb";
+import { randomUUID } from "crypto";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
+const tableName = "Team57";
+
+app.get('/answers/:id', async (req, res) => {
+  const response = await dynamo.send(
+    new GetCommand({
+      TableName: tableName,
+      Key: {
+        id: req.params.id,
+      },
+    })
+  );
+  res.json(response.Item);
+});
+
+app.put('/answers', async (req, res) => {
+  const id = randomUUID();
+  await dynamo.send(
+    new PutCommand({
+      TableName: tableName,
+      Item: {
+        id,
+        answer: '',
+      },
+    })
+  );
+  res.json({ id });
+});
+
+app.post('/answers', async (req, res) => {
+  const data = JSON.parse(req.body);
+  const response = await dynamo.send(
+    new UpdateCommand({
+      TableName: tableName,
+      Key: { id: data.id },
+      UpdateExpression: "set answer = :a",
+      ExpressionAttributeValues: {":a": data.answer},
+      ReturnValues: "UPDATED_NEW"
+    })
+  );
+  res.json(response.Attributes);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
